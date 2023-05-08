5e15ef309247d5b9d3d833d09d05e8cca36cc9d1
sael-lang <hasnain6613@gmail.com>
2023-05-08 15:29:56 -0700

    first commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Ahmad
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frame1 frame;
+        frame = new frame1();
+        frame.setVisible(true);
+    }
+    
+}
